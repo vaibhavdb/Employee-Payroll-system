07452abda8c848ae02c5dc32a64614771eefc54a
Your Name <vaibhav.bijwe@skillnetinc.com>
2026-07-17 13:28:37 +0530

    initial commit entity

--- employee-payroll-system/src/main/java/com/vaibhav/payroll/EmployeePayrollSystemApplication.java
+++ employee-payroll-system/src/main/java/com/vaibhav/payroll/EmployeePayrollSystemApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmployeePayrollApplication {
+public class EmployeePayrollSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollApplication.class, args);
+		SpringApplication.run(EmployeePayrollSystemApplication.class, args);
 	}
 
 }
